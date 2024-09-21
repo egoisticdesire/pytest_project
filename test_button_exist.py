f03a1f85425a2f5a9b35c1dfa4df63b1deb6c6eb
egoistic_desire <kfkfjqjq@mail.ru>
2024-09-21 20:50:02 +0400

    upd actions

--- test_button_exist.py
+++ test_button_exist.py
@@ -16,3 +16,8 @@ def driver():
 def test_button_exist(driver):
     driver.get('https://www.qa-practice.com/elements/button/simple')
     assert driver.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+
+def test_second_button_exist(driver):
+    driver.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert driver.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
